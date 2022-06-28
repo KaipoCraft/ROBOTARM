5c999ed216dd9d650ba983a18bb06a276aaaf592
Will Harris <Xuis@users.noreply.github.com>
2022-06-27 19:29:11 -0600

    Added comments to sketch.js

--- sketch.js
+++ sketch.js
@@ -7,17 +7,21 @@
 import Mediapipe from "./MediapipeHands.js";
 import { scalePoints, calculateDistance } from "./functions.js";
 
+// enable camera
 const videoElement = document.getElementsByClassName('input_video')[0];
 
 let width = 1280;
 let height = 720;
 let mp = new Mediapipe();
+// place to put info about points on hand
 let indexPoints = [];
 let thumbPoints = [];
 let wristPoint = [];
 
+// angle that is exported to the arduino for the servo
 let degrees;
 
+// everything the serial controller needs
 var serial;
 var portName = '0COM3';
 var inData;
@@ -39,6 +43,7 @@ window.setup = function() {
   
   serial.open(portName);
 
+  // color of the hand
   mp.color = [255, 255, 255];
 }
 
@@ -51,25 +56,32 @@ window.draw = function() {
 
   // somehow make a way to find the maximum distance between thumb tip and index tip?
 
+  // takes the points from mediapipe, and scales them to the canvas
   for (point in mp.index) {
     indexPoints.push(scalePoints(mp.index[point]));
     thumbPoints.push(scalePoints(mp.thumb[point]));
     wristPoint.push(scalePoints(mp.wrist[point]));
   }
 
+
   background(50);
   fill(255);
+  // display the data coming back from the arduino to make sure it's the same as when it was sent
   text("incoming value: " + inData, 30, 50);
 
   // Actions to do if the hand is detected
   if (indexPoints[0] != null) {
-
+    
+    // get the distance between the index and thumb
     let distance = calculateDistance(indexPoints[0], thumbPoints[0]);
 
+    // scales the distance between 0 and 180 #TODO
     let angle = int(map(indexPoints[0].y, 0, windowHeight, 0, 180));
 
+    // #TODO want degrees to have multiple angles for multiple motors
     degrees = angle;
 
+    // make sure the angle stays within bounds (0< x <180)
     console.log(degrees);
     // if (angle <= 180) {
     //   degrees = angle;
@@ -93,8 +105,10 @@ window.draw = function() {
   //    [2] = wrist;
   //    [3] = claw; distance between indextip and thumbtip
 
+  // actually sends the data to the arduino
   updateAngle();
 
+  // resets the hand points every time
   indexPoints = [];
   thumbPoints = [];
   wristPoint = [];
